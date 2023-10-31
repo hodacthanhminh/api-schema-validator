52b11ccc1c1e705984bf7d25f806482ee6beb667
hodacthanhminh <hodacthanhminh@gmail.com>
2023-10-31 16:16:32 +0700

    :sparkles: feat: create simple api.spec.ts

---
+++ src/test/api.spec.ts
@@ -0,0 +1,13 @@
+import { describe, test } from "vitest";
+import { validateDataSchema, validateTypeSchema } from "@/helpers/jsonSchemaValidator.js";
+import { requestMock } from "@/test/__mocks__/apiResponse.js";
+
+describe("JSON Schema validate", () => {
+    test("Type should be valid JSON Schema", validateTypeSchema({ type: "IElementsQueryResult" }));
+    test(
+        "API response should match JSON Schema", async () => {
+            const response = await requestMock();
+            validateDataSchema(response, { type: "IElementsQueryResult" });
+        }
+    );
+});
